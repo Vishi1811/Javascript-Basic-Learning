e2b195739e82f0021102f56b85ce01df4450470e
Vishal Yadav <110190901+Vishi1811@users.noreply.github.com>
2025-01-26 20:27:06 +0000

    commit

---
+++ Eventobject/Asyn/one.js
@@ -0,0 +1,6 @@
+//<-------------------- The setTimeout()--------------------------> 
+// The setTimeout() function is used to add delay or scheduling the execution of a specific function after a certain period. It’s a key feature of both browser environments and Node.js, enabling asynchronous behavior in code execution.Syntax:
+// setTimeout(function, delay(in ms), arg1, arg2, ...);
+setTimeout(function() {
+    console.log("Vishal")
+}, 2000);
